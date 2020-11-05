ca26e5cca3f097e30e4d6dca9db1ae5ca24f1ea2
prateekstark <gargprateek35@gmail.com>
2020-11-06 01:08:00 +0530

    adding ste training

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Training Binary Neural Networks
+<p align="left">
+<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+</p>
+
 This is our entry for the ML Reproducibility Challenge 2020.
--- simple_binn.py
+++ simple_binn.py
@@ -1,30 +1,53 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from BiNN.layers import BinaryLinear
+from STE.MLP import BinaryConnect
+from dataloader import Dataset
+import logging
+from torchsummary import summary
+from tqdm import tqdm
 
 """
 Pointers: 
 	1) We need to make sure that while we use batch_norm layer, we must use a batch size of more than 1.
 """
 
-
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-        self.binary_fc1 = BinaryLinear(in_features=784, out_features=300, bias=False)
-        self.bn_layer1 = nn.BatchNorm1d(300)
-        self.output_layer = BinaryLinear(in_features=300, out_features=10, bias=False)
-        self.output_bn_layer = nn.BatchNorm1d(num_features=10)
-
-    def forward(self, x):
-        x = self.binary_fc1(x)
-        x = self.bn_layer1(x)
-        x = self.output_layer(x)
-        return self.output_bn_layer(x)
-
-
 if __name__ == "__main__":
-    net = Network()
-    input_val = torch.randn(2, 784)
-    print(net(input_val))
+    if(torch.cuda.is_available()):
+        device = 'cuda:0'
+    else:
+        device = 'cpu'
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    dataset = Dataset('mnist', data_augmentation=False, validation_split=0.1)
+    trainloader, testloader, valloader = dataset.get_dataloaders(batch_size=100)
+    net = BinaryConnect(784, 10).to(device)
+    
+    summary(net, input_size=(100, 784), device='cuda')
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    
+    epochs = 100
+
+    milestones = [int(epochs / 2), int(epochs / 12), 150, 250, 350, 450]
+
+    for epoch in range(epochs):
+        result = []
+        for inputs, labels in tqdm(trainloader):
+            optimizer.zero_grad()
+            output = net(inputs.to(device))
+            loss = criterion(output, labels.to(device))
+            loss.backward()
+            for p in net.parameters():
+                if(hasattr(p, 'latent_')):
+                    p.data.copy_(p.latent_)
+            optimizer.step()
+            for p in net.parameters():
+                if(hasattr(p, 'latent_')):
+                    p.latent_.copy_(p.data)
+            pred = output.argmax(dim=1, keepdims=True)
+            correct = pred.eq(labels.to(device).view_as(pred)).sum().item()
+            # print(correct)
+            result.append(correct)
+        print(sum(result) / len(result))
